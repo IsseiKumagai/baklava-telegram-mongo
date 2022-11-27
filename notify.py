66525814fb1427b78da169a334d5052989c5cde2
Issei Kumagai <48951453+IsseiBlockchain@users.noreply.github.com>
2022-11-27 11:49:28 +0800

    Added a telegram bot

--- notify.py
+++ notify.py
@@ -1,34 +1,19 @@
-#  Copyright 2019 Gabriele Valvano
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 
 import telegram_notifier
 import logging
 import argparse
 import socket
 import json
-from datetime import datetime
+import datetime
 import os
 
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 
-
 # telegram bot ---
 TELEGRAM_TOKEN_ID = "5760680368:AAHLf-ZTdshgYLExYgiqrx60altD02k11Kg"  # token-id
 TELEGRAM_CHAT_ID = "5331052190" # chat-id
 # ----------------
-
 
 parser = argparse.ArgumentParser(description='Notifier.')
 parser.add_argument("--message", type=str, help='Message for the notifier.', default='hello from issei')
@@ -37,11 +22,10 @@ parser.add_argument("--chat_id", type=str, help='Chat ID for the chat bot.', def
 parser.add_argument("--hostname", type=str, help='Name of the server running the task.', default=hostname)
 parser.add_argument("--ip", type=str, help='IP address of the server running the task.', default=IPAddr)
 
-
-def main():
+def main(text):
 
     args = parser.parse_args()
-    text = args.message
+    text = text
     hostname = args.hostname
     ip = args.ip
 
@@ -51,22 +35,47 @@ def main():
 
     # - - - - - - - - - -
     # Telegram notification:
-    msg = "Automatic message from host: '{hostname}', at: {ip}\n{separator}\n" \
-          "<b>MESSAGE: </b>\n<pre> {text} </pre>".format(separator=' -' * 10, text=text, hostname=hostname, ip=ip)
+    # msg = "Automatic message from host: '{hostname}', at: {ip}\n{separator}\n" \
+    #       "<b>MESSAGE: </b>\n<pre> {text} </pre>".format(separator=' -' * 10, text=text, hostname=hostname, ip=ip)
+    msg = text
     telegram_notifier.basic_notifier(logger_name='training_notifier',
                                      token_id=token_id,
                                      chat_id=chat_id,
                                      message=msg,
                                      level=logging.INFO)
 # just calls the `main` function above
+
+
 if __name__ == '__main__':
+
+    STABLE_COIN_ADDRESSES = {
+        "USDC.e": "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
+        "USDC": "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E"
+    }
     stable_coin_reserve = open("stable_coin_reserve.json")
     stable_coin_distribution = open("stable_coin_distribution.json")
     stable_coin_reserve_data = json.load(stable_coin_reserve)
     stable_coin_distribution_data = json.load(stable_coin_distribution)
-    today_date = datetime.today().strftime('%Y-%m-%d')
-    x = open("/Users/pundix/PycharmProjects/Baklava_20221104/stable_coin_distribution.json")
-    print(os.getcwd())
-    # returns JSON object as
-    # a dictionary
-    main()
+    total_sum_stable_coin = {key: sum(sum_in_contract for address, sum_in_contract in value.items()) for key, value in stable_coin_reserve_data.items()}
+    today_date = datetime.datetime.today()
+
+    message_to_send = ""
+
+    for day in range(0, 5):
+        new_date = (today_date + datetime.timedelta(days=day)).date().strftime('%Y-%m-%d')
+        message_to_send += "\n" + "----------" + new_date + "----------" + "\n"
+        print(message_to_send)
+        for stable_coin_name, stable_coin_address in STABLE_COIN_ADDRESSES.items():
+            amount_to_distribute_for_date = stable_coin_distribution_data["accumulative"][stable_coin_address]
+            if new_date in amount_to_distribute_for_date:
+                total_money_left = total_sum_stable_coin[stable_coin_name] - amount_to_distribute_for_date[new_date]
+                if total_money_left < 0:
+                    message_to_send += f"{stable_coin_name}: ${total_money_left * -1}\n"
+                else:
+                    message_to_send += f"{stable_coin_name}: Enough funds available\n"
+            else:
+                message_to_send += f"{stable_coin_name}: Enough funds available\n"
+
+        print(message_to_send)
+
+    main(message_to_send)
